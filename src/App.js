175e38b3169c2c83f66d3f9358712baddd7f4ca3
bharathp9 <pbharathkumar86@gmail.com>
2018-09-21 13:30:56 +0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const Hello = () => <h2>Hello World from Training</h2>
-const Image = (props) => {
-  let { src1, title } = props;
-  src1 = src1 || require('./pic_trulli.jpg');
-  title = title || 'Default Picture';
-  return(
-    <img src={src1} alt={title} width="300" height="333" />
-  )
-}
+import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Training</h1>
-        </header>
-        {/*<p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-    </p> */}
-        <Hello/>
-        <Image />
-        <Image src1='https://www.w3schools.com/html/img_girl.jpg' title='girl' />
-        <Image src1='https://www.w3schools.com/html/img_chania.jpg' title='chania' />
+    state = {
+        characters: []
+    };
 
-      </div>
-    );
-  }
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
